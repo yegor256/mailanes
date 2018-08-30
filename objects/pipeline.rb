044a8e3e290d747b4473cafd448d453db2b2eb61
Yegor Bugayenko <yegor256@gmail.com>
2018-08-30 13:09:55 +0300

    #10 relax

--- objects/pipeline.rb
+++ objects/pipeline.rb
@@ -55,8 +55,14 @@ class Pipeline
       recipient = Recipient.new(id: r['rid'].to_i, pgsql: @pgsql)
       delivery = deliveries.add(campaign, letter, recipient)
       if letter.yaml['relax']
-        days, hours, minutes = letter.yaml['relax'].split(':')
-        delivery.relax(Time.now + (days.to_i * 24 * 60 + hours.to_i * 60 + minutes.to_i) * 60)
+        time = Time.now
+        if letter.yaml['relax'] =~ /[0-9]+:[0-9]+:[0-9]+/
+          days, hours, minutes = letter.yaml['relax'].split(':')
+          time += (days.to_i * 24 * 60 + hours.to_i * 60 + minutes.to_i) * 60
+        elsif letter.yaml['relax'] =~ /[0-9]{2}-[0-9]{2}-[0-9]{4}/
+          time = Time.parse(letter.yaml['relax'])
+        end
+        delivery.relax(time)
       end
       postman.deliver(delivery)
     end
