2e46fd817b3f119db88be3ceba9e817962946973
Yegor Bugayenko <yegor256@gmail.com>
2018-09-13 13:24:27 +0300

    #35 typos

--- test/test_ago.rb
+++ test/test_ago.rb
@@ -30,5 +30,6 @@ class AgoTest < Minitest::Test
   def test_hour_ago
     assert_equal('1 hour ago', Ago.new(Time.now - 60 * 60).to_s)
     assert_equal('6 hours ago', Ago.new(Time.now - 5 * 60 * 60 - 60 * 60 - 30).to_s)
+    assert_equal('1 week ago', Ago.new(Time.now - 9 * 24 * 60 * 60 - 5 * 60 * 60 - 60 * 60 - 30).to_s)
   end
 end
