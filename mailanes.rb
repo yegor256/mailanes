9eea5504c52e3ac6eaf7e09f85bc9b77e60b89c4
Yegor Bugayenko <yegor256@gmail.com>
2025-12-22 04:51:12 +0300

    Update log level to REGULAR in configuration.

--- mailanes.rb
+++ mailanes.rb
@@ -75,7 +75,7 @@ configure do
   set :config, config
   set :logging, true
   set :server_settings, timeout: 25
-  set :log, Loog::VERBOSE
+  set :log, Loog::REGULAR
   set :zache, Zache.new(dirty: true)
   set :glogin, GLogin::Auth.new(
     config['github']['client_id'],
